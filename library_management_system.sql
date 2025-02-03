d0acc8648167c3476ec7458252cbeb2355d663c5
ramiru-nonis <ramirunonis2006@gmail.com>
2025-02-03 14:42:52 +0530

    done

--- library_management_system.sql
+++ library_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 01, 2025 at 08:11 AM
+-- Generation Time: Feb 03, 2025 at 10:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,13 +36,6 @@ CREATE TABLE `admin_table` (
   `role` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `admin_table`
---
-
-INSERT INTO `admin_table` (`Id`, `name`, `password`, `email`, `phoneNumber`, `role`) VALUES
-(1, 'lesandu', 'lesandu123', 'lesanduluthmeen@gmail.com', 740713025, 'Admin');
-
 -- --------------------------------------------------------
 
 --
@@ -57,20 +50,6 @@ CREATE TABLE `books_table` (
   `ISBN` varchar(50) NOT NULL,
   `URL` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `books_table`
---
-
-INSERT INTO `books_table` (`ID`, `Title`, `Author`, `Year`, `ISBN`, `URL`) VALUES
-(7, 'By - ways of war', 'jeffery Roche', 1986, '9783029464046', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\001.jpg'),
-(8, 'Records of Steam Boiler Explosions', 'Edward Bindon Marten', 2015, '9786437878856', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\016.jpg'),
-(9, 'KIM', 'unknown', 1985, '9782025774296', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\002.jpg'),
-(10, 'Thorn in my side', 'C J S K U S E', 1999, '9781088797051', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\006.jpg'),
-(11, 'mahanuwara rajadhaniye', 'manel ranasinghe', 2006, '9785030910284', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\042.jpg'),
-(12, 'Effel Tower', 'Robert M .Vogel', 1956, '9789697678624', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\018.jpg'),
-(13, 'boss', 'pakaya', 1234, '9784471851965', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\044.jpg'),
-(14, 'Farm engines and how to run', 'james h stephenson', 1956, '9783508308519', 'C:\\Users\\lesandu\\OneDrive - Asia Pacific Institute of Information Technology\\Pictures\\images\\021.jpg');
 
 -- --------------------------------------------------------
 
@@ -88,21 +67,6 @@ CREATE TABLE `borrowed_records` (
   `ReturnedDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `borrowed_records`
---
-
-INSERT INTO `borrowed_records` (`BorrowedId`, `BookId`, `UserId`, `BorrowDate`, `DueDate`, `IsReturned`, `ReturnedDate`) VALUES
-(1, 8, 1, '2025-01-31', '2025-02-14', 1, '2025-01-31'),
-(2, 8, 1, '2025-01-31', '2025-02-14', 1, '2025-02-21'),
-(3, 10, 1, '2025-01-31', '2025-02-14', 1, '2025-02-01'),
-(4, 12, 1, '2025-02-01', '2025-02-15', 1, '2025-02-01'),
-(5, 9, 2, '2025-02-01', '2025-02-15', 0, '0000-00-00'),
-(6, 13, 2, '2025-02-01', '2025-02-15', 0, '0000-00-00'),
-(7, 14, 2, '2025-02-01', '2025-02-15', 0, '0000-00-00'),
-(8, 11, 2, '2025-02-01', '2025-02-15', 0, '0000-00-00'),
-(9, 7, 1, '2025-02-01', '2025-02-15', 1, '2025-02-01');
-
 -- --------------------------------------------------------
 
 --
@@ -117,13 +81,6 @@ CREATE TABLE `librarian_table` (
   `phoneNumber` int(10) NOT NULL,
   `role` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `librarian_table`
---
-
-INSERT INTO `librarian_table` (`Id`, `name`, `password`, `email`, `phoneNumber`, `role`) VALUES
-(4, 'Librarian', 'lesandu12', 'Librarian@gmail.com', 1234567890, 'Librarian');
 
 -- --------------------------------------------------------
 
@@ -141,13 +98,6 @@ CREATE TABLE `overdue_table` (
   `PaidStatus` tinyint(1) NOT NULL,
   `PaidDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `overdue_table`
---
-
-INSERT INTO `overdue_table` (`OverdueId`, `BorrowedId`, `UserID`, `BookID`, `OverdueDays`, `FineAmount`, `PaidStatus`, `PaidDate`) VALUES
-(1, 2, 1, 8, 7, 35, 1, '2025-02-01');
 
 -- --------------------------------------------------------
 
@@ -179,14 +129,6 @@ CREATE TABLE `student_table` (
   `phoneNumber` int(10) NOT NULL,
   `role` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `student_table`
---
-
-INSERT INTO `student_table` (`Id`, `name`, `password`, `email`, `phoneNumber`, `role`) VALUES
-(1, 'GuestUser', 'lesandu1', 'lesanduluthmeen@gmail.com', 740713025, 'Student'),
-(2, 'lesaBoss', 'lesandu12', 'lesandu@gmail.com', 740713025, 'Student');
 
 --
 -- Indexes for dumped tables
@@ -242,31 +184,31 @@ ALTER TABLE `student_table`
 -- AUTO_INCREMENT for table `admin_table`
 --
 ALTER TABLE `admin_table`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `books_table`
 --
 ALTER TABLE `books_table`
-  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ID` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `borrowed_records`
 --
 ALTER TABLE `borrowed_records`
-  MODIFY `BorrowedId` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `BorrowedId` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `librarian_table`
 --
 ALTER TABLE `librarian_table`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `overdue_table`
 --
 ALTER TABLE `overdue_table`
-  MODIFY `OverdueId` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `OverdueId` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reservation_table`
@@ -278,7 +220,7 @@ ALTER TABLE `reservation_table`
 -- AUTO_INCREMENT for table `student_table`
 --
 ALTER TABLE `student_table`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
